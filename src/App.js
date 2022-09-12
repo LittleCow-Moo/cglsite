3b795dad2b0f48945f78380dcf5edb46a998c9c1
Charlie <kiwichang2017@gmail.com>
2022-09-12 21:33:20 +0800

    hmm

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import icon from './icon.png';
 import grassBlock from './grassBlock.png';
 import sword from './sword.png';
 import skulkSensor from './skulkSensor.gif';
-import styled from 'styled-components';
-const Black = styled.p`
-  color: black !important;
-`
 
 function App() {
   return (
     <>
       <div id="title">
-        <img src={icon} alt="牛牛" width="55px" />
+        <img src={icon} alt="牛牛" /><br/>
         牛牛的草原
       </div>
       <div id="root">
@@ -20,7 +16,8 @@ function App() {
         我們有創造、生存和古城生存伺服器!
         <div className="onehundredpercentuwu" id="cre">
           <table style={{width:'100%',height:'100%',fontSize:'30px',whiteSpace:'pre-wrap',color:'white'}}>
-            <tr>
+            <tbody>
+              <tr>
                 <td>
                   <img src={grassBlock} width="30%" alt="草方塊"/><br/>
                   創造伺服器
@@ -31,11 +28,13 @@ function App() {
                   盡情地創造！
                 </td>
               </tr>
+            </tbody>
           </table>
         </div>
         <div className="onehundredpercentuwu" id="surv">
           <table style={{width:'100%',height:'100%',fontSize:'30px',whiteSpace:'pre-wrap',color:'white'}}>
-            <tr>
+            <tbody>
+              <tr>
                 <td>
                   一片廣闊的草原，<br/>
                   你可以在這裡<br/>
@@ -46,11 +45,13 @@ function App() {
                   生存伺服器
                 </td>
               </tr>
+            </tbody>
           </table>
         </div>
         <div className="onehundredpercentuwu" id="ancient">
           <table style={{width:'100%',height:'100%',fontSize:'30px',whiteSpace:'pre-wrap',color:'white'}}>
-            <tr>
+            <tbody>
+              <tr>
                 <td>
                   <img src={skulkSensor} width="30%" alt="尋聲感測器"/><br/>
                   古城生存伺服器
@@ -58,14 +59,32 @@ function App() {
                 <td>
                   在漆黑的遠古之城生存，<br/>
                   小心不要觸發尖嘯器，<br/>
-                  不然就會召喚出Warden！
+                  不然就會召喚出Warden！<br/><br/>
+                  感謝<a href="https://youtube.com/c/Onityan">鬼鬼</a>的<a href="https://youtu.be/ZvKw9OVza88">遠古之城生存系列</a>提供靈感
                 </td>
               </tr>
+            </tbody>
           </table>
         </div>
-        虛空創造
-        突破限制的1024格高虛空地圖，<br/>
-        讓你盡情地創造！
+        <div className="onehundredpercentuwu" id="void">
+          <table style={{width:'100%',height:'100%',fontSize:'30px',whiteSpace:'pre-wrap',color:'white'}}>
+            <tbody>
+              <tr>
+                <td>
+                  [即將到來]<br/>
+                  突破限制的<br/>
+                  1024格高虛空地圖，<br/>
+                  讓你盡情地創造！<br/><br/>
+                  感謝<a href="https://youtube.com/c/Koi戀">Koi.戀</a>的<a href="https://youtu.be/rPxMghe14SA">高度擴充Addon</a>
+                </td>
+                <td>
+                  <img src={icon} width="30%" alt="只是佔位用AwA"/><br/>
+                  虛空創造伺服器
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
