2710c14270315babe137686266d38069f2a64f32
BuildTools <unconfigured@null.spigotmc.org>
2023-01-15 17:08:58 +0800

    remake site using Portal website template

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const getWidth = () => {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  )
+}
+
+let t
+$(document).ready(() => {
+  t = $(".ip").html()
+})
+
+$(document).on("click", ".ip", () => {
+  let copy = document.createElement("textarea")
+  copy.style.position = "absolute"
+  copy.style.left = "-99999px"
+  copy.style.top = "0"
+  copy.setAttribute("id", "ta")
+  document.body.appendChild(copy)
+  copy.textContent = t
+  copy.select()
+  document.execCommand("copy")
+  setTimeout(() => {
+    $(".ip").html(t)
+    var copy = document.getElementById("ta")
+    copy.parentNode.removeChild(copy)
+  }, 800)
+})
+
+$(document).ready(() => {
+  let ip = $(".sip").attr("data-ip")
+  let port = $(".sip").attr("data-port")
+  if (port == "" || port == null) port = "25565"
+  if (ip == "" || ip == null)
+    return console.error(
+      "Error fetching player count - is the IP set correctly in the HTML?"
+    )
+  updatePlayercount(ip, port)
+  setInterval(() => {
+    updatePlayercount(ip, port)
+  }, 60000)
+})
+
+const updatePlayercount = (ip, port) => {
+  $.get(`https://mcapi.us/server/status?ip=${ip}&port=${port}`, (result) => {
+    if (result.hasOwnProperty("players")) {
+      $(".sip").html(result.players.now)
+    } else {
+      $(".playercount").html("伺服器不在線上!")
+    }
+  })
+}
